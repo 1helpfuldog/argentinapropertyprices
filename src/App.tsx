90c7997f64638ae20b47643d88f3924aabb7a71d
1helpfuldog <knutbernhardt@outlook.com>
2026-09-06 15:20:20 +0200

    Wrap app in language provider and sound dock

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Landing } from "./pages/Landing";
 import { AmbaMap } from "./pages/AmbaMap";
+import { LangProvider } from "./lib/lang";
+import { SoundDock } from "./components/SoundDock";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/amba" element={<AmbaMap />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <LangProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/amba" element={<AmbaMap />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+      <SoundDock />
+    </LangProvider>
   );
 }
